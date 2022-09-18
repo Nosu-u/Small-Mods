df1e8ed2d3b584a72a9205782f99015d10e3a81d
Nosu♡ <93537876+Nosu-u@users.noreply.github.com>
2022-09-19 00:26:57 +0200

    woah

---
+++ NoRotation/main.cpp
@@ -0,0 +1,19 @@
+#include <Geode.hpp>
+
+USE_GEODE_NAMESPACE();
+
+class $(PlayerObject) {
+	virtual void setRotation(float love) {
+	love = 0.0;
+
+	return PlayerObject::setRotation(love);
+	}
+};
+
+
+
+
+
+
+
+
