b16abfea7f64745a9562945cd9bfc0bbe40c7176
Nosu♡ <93537876+Nosu-u@users.noreply.github.com>
2022-10-12 16:17:34 +0200

    not the worst method anymore

---
+++ HideAttempts/main.cpp
@@ -0,0 +1,13 @@
+#include <Geode.hpp>
+
+USE_GEODE_NAMESPACE();
+
+CCLabelBMFont* m_attemptLabel;
+
+class $(PlayLayer) {
+    virtual void update(float dt) {
+        m_attemptLabel->setVisible(false);
+    PlayLayer::update(dt);
+    
+    }
+};
