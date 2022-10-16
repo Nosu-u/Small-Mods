b1c2234797a230a9dee53af31426d8331b6a2733
Nosu♡ <93537876+Nosu-u@users.noreply.github.com>
2022-10-16 23:22:49 +0200

    smooth af

---
+++ NoTransition/main.cpp
@@ -0,0 +1,9 @@
+#include <Geode.hpp>
+
+USE_GEODE_NAMESPACE();
+
+class $(CCTransitionFade) {
+    static CCTransitionFade* create(float, CCScene* scene) {
+            return CCTransitionFade::create(.0f, scene);
+    }
+};
